21e3e05e4ed73a81a1264beb17002e1617a6361d
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-15 14:09:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
 
+using namespace std;
 int main(){
+      char a[10][20];
+    char b[1][20];
+    for (int i = 0; i < 10; ++i) {                        
+        for (int j = 0; j < 20; ++j) {
+            a[i][j]=' ';
+            b[0][j]=' ';
+        }
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        cin >> a[i];
+    }
+
+    cin >> b[0];
+
+    if(nome(a,b)==-1){
+        cout<< "non presente";
+    }else{
+        cout << nome(a,b);
+    }
   return 0;
 }
