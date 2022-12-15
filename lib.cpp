889320c7799c71775aef6ba51e68e13c526c4ec7
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-15 14:08:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
-
+#include "lib.h"
+int nome( char a[10][20], char b[1][20]){
+     for(int i= 0; i< 10; i++) {
+          if (b[0][0] == a[i][0]) {
+             for (int j= 0; j < 20; j++) {
+                 if (b[0][j] != a[i][j]) {
+                     break;
+                 }
+                 if(j==19){
+                     return i;
+                 }
+             }
+         }
+     }
+     return -1;
+}
